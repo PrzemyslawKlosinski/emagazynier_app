b8f186823c463e7349dc026c7cda2ec915a5a0e7
Przemyslaw Klosinski <przemyslaw.klosinski.noSpam@gmail.com>
2013-01-09 14:09:18 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmagazynierApp::Application.config.secret_token = '29e03085a456448a2bfe98b9395a1fbda183b536aed023018390b3712d02bd703bc5de9069ee26564b23047fc0e9065184116b6dcf57bb70e9948c9ebff56716'
