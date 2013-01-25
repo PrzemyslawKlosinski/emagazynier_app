f5d9d3ceaaae97de5ae2a92944fd0153819df710
Przemyslaw Klosinski <przemyslaw.klosinski.noSpam@gmail.com>
2013-01-25 18:32:37 +0100

    change registration email link to heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ActionMailer::Base
     # widocznosc zmiennej user w welcome_email.erb.html
 	@user = user
 	enc = Base64.encode64(@user.password_digest)
-	@url  = "http://0.0.0.0:3000/aktywacja/#{enc}"
+	@url  = "https://emagazynier.herokuapp.com/aktywacja/#{enc}"
     email_with_name = "#{@user.name} <#{@user.email}>"
     mail(:to => email_with_name, :subject => "Witaj w super serwisie eMagazynier")
   end
--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 
 
 # if ENV['RAILS_ENV'] == "production"
-  ActiveSupport::Deprecation.silenced = true
+#   ActiveSupport::Deprecation.silenced = true
 # end
 
 
