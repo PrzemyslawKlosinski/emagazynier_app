8cb03c890816a94c9e37424b810e2a35eea7bb51
Przemyslaw Klosinski <przemyslaw.klosinski.noSpam@gmail.com>
2013-02-24 14:09:47 +0100

    routing error

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require File.expand_path('../application', __FILE__)
 #   ActiveSupport::Deprecation.silenced = true
 # end
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+# Encoding.default_external = Encoding::UTF_8
+# Encoding.default_internal = Encoding::UTF_8
 
 # Initialize the rails application
 EmagazynierApp::Application.initialize!
